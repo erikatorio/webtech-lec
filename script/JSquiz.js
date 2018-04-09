1bc079acbd959282203f56cf2948a9a05a5bd5c2
erikatorio <2160120@slu.edu.ph>
2018-04-09 12:59:58 +0800

    modified css files, added more content

--- script/JSquiz.js
+++ script/JSquiz.js
@@ -1,13 +1,183 @@
 (function() {
     const JSQuestions = [
         {
-            question: "",
-            answers: {
-              a: "",
-              b: "",
-              c: ""
-            },
-            correctAnswer: ""
+          question: "In what HTML element do we put the javascript?",
+          answers: {
+            a: "<javascript>",
+            b: "<java>",
+            c: "<script>"
+          },
+          correctAnswer: "c"
+        },
+        {
+          question: "This is a W3C standard that defines a standard for accessing documents.",
+          answers: {
+            a: "Document Operation Model",
+            b: "Document Object Model",
+            c: "Document Access Model"
+          },
+          correctAnswer: "b"
+        },
+        {
+          question: "This tag is the alternative for the browsers that does not support scripting.",
+          answers: {
+            a: "<noscript>",
+            b: "<nscript>",
+            c: "<nscript>",
+          },
+          correctAnswer: "a"
+        },
+        {
+          question: "This method retrieve references to node in the DOM that returns an element that has an id attribute.",
+          answers: {
+            a: "getElementById()",
+            b: "getElementId()",
+            c: "getElement()"
+          },
+          correctAnswer: "a"
+        },
+      {
+          question: "This method returns a collection of all elements in the document that thas a specified tag name.",
+          answers: {
+            a: "getElementsByTName()",
+            b: "getElementsByTagName()",
+            c: "getElementsByTag()"
+          },
+          correctAnswer: "b"
+        },
+      {
+          question: "This property returns the first child node.",
+          answers: {
+            a: "1Child",
+            b: "fChild",
+            c: "firstChild"
+          },
+          correctAnswer: "c"
+        },
+      {
+          question: "This property returns the last child node.",
+          answers: {
+            a: "lChild",
+            b: "lastChild",
+            c: "LastChild"
+          },
+          correctAnswer: "b"
+        },
+      {
+          question: "The programming language of a HTML and Web.",
+          answers: {
+            a: "Java",
+            b: "C++",
+            c: "JavaScript"
+          },
+          correctAnswer: "c"
+        },
+      {
+          question: "Core language of Java Script",
+          answers: {
+            a: "EDMAScript",
+            b: "ESMAScript",
+            c: "ECMAScript"
+          },
+          correctAnswer: "c"
+        },
+      {
+          question: "This method removes a specified child.",
+          answers: {
+            a: "removeChild()",
+            b: "removeC()",
+            c: "rChild()"
+          },
+          correctAnswer: "a"
+        },
+      {
+          question: "This method appends the node as the last child.",
+          answers: {
+            a: "aChild(0",
+            b: "appendChildNode()",
+            c: "appendChild()"
+          },
+          correctAnswer: "c"
+        },
+      {
+          question: "Inserts a node right before an existing node",
+          answers: {
+            a: "insertNodeBefore()",
+            b: "insertBefore()",
+            c: "insertNode()"
+          },
+          correctAnswer: "b"
+        },
+      {
+          question: "Provide the correct syntax fore reffering to a external javascript called example.js",
+          answers: {
+            a: "<javascript src='example.js'",
+            b: "<script src='example.js'>",
+            c: "<java src='example.js'>"
+          },
+          correctAnswer: "b"
+        },
+      {
+          question: "True/False: The external javascript must contain the <script> tag",
+          answers: {
+            a: "True",
+            b: "False",
+          },
+          correctAnswer: "a"
+        },
+      {
+          question: "This method creates an element node with the specified name.",
+          answers: {
+            a: "createNodeElement()",
+            b: "createNode()",
+            c: "createElement()"
+          },
+          correctAnswer: "c"
+        },
+      {
+          question: "The method returns the first element that matches a specified CSS selector(s) in the document.",
+          answers: {
+            a: "getElement()",
+            b: "querySelector()",
+            c: "returnElement()"
+          },
+          correctAnswer: "b"
+        },
+      {
+          question: "This method creates a copy of a node, and returns the copied node.",
+          answers: {
+            a: "cloneNode()",
+            b: "copyNode()",
+            c: "createNode()"
+          },
+          correctAnswer: "a"
+        },
+      {
+          question: "This method returns a collection of all elements in the document with the specified class name, as a NodeList object.",
+          answers: {
+            a: "getElementsByClassName() ",
+            b: "getElementsByName()",
+            c: "getElementsByClass()"
+          },
+          correctAnswer: "a"
+        },
+      {
+          question: "This property returns the node type, as a number, of the specified node.",
+          answers: {
+            a: "nodeNumberType",
+            b: "nodeClass",
+            c: "nodeType"
+          },
+          correctAnswer: "c"
+        },
+        {
+          question: "This method adopts a node from another document.",
+          answers: {
+            a: "adoptNodeElement()",
+            b: "adoptNode()",
+            c: "adoptChild()"
+          },
+          correctAnswer: "b"
         }
     ];
   
