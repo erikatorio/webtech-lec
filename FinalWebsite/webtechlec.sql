607157512dd8637b5289fe1211df609ba915babd
erikatorio <2160120@slu.edu.ph>
2018-05-02 20:03:40 +0800

    updated website database implementation

--- FinalWebsite/webtechlec.sql
+++ FinalWebsite/webtechlec.sql
@@ -28,6 +28,8 @@ CREATE TABLE `accounts` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0',
+  `points` int(11) DEFAULT NULL,
+  `level` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`username`),
   UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -39,8 +41,116 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES ('erikatorio','erika',0),('jessietabilisma','jessie',0),('kenancea','kenan',0),('sonnyby','sonny',0);
+INSERT INTO `accounts` VALUES ('erikatorio','erika',1,NULL,1),('jessietabilisma','jessie',0,NULL,1),('kenancea','kenan',0,NULL,1),('sonnyby','sonny',0,NULL,1);
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `choices`
+--
+
+DROP TABLE IF EXISTS `choices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `choices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `choice` varchar(100) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id_idx` (`question_id`),
+  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `choices`
+--
+
+LOCK TABLES `choices` WRITE;
+/*!40000 ALTER TABLE `choices` DISABLE KEYS */;
+INSERT INTO `choices` VALUES (1,'Personal Home Page',1),(2,'Hypertext Preprocessor',1),(3,'Process Home Preprocessor',1),(4,'1993',2),(5,'1994',2),(6,'1995',2);
+/*!40000 ALTER TABLE `choices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(100) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `correctanswer` varchar(45) NOT NULL,
+  PRIMARY KEY (`question_id`),
+  KEY `quiz_id_idx` (`quiz_id`),
+  CONSTRAINT `quiz_id` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`quiz_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'What does PHP stand for?',1,'b'),(2,'When was PHP developed?',1,'b');
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz`
+--
+
+DROP TABLE IF EXISTS `quiz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz` (
+  `quiz_id` int(11) NOT NULL AUTO_INCREMENT,
+  `quiz_name` varchar(45) NOT NULL,
+  `total_points` int(11) NOT NULL,
+  PRIMARY KEY (`quiz_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz`
+--
+
+LOCK TABLES `quiz` WRITE;
+/*!40000 ALTER TABLE `quiz` DISABLE KEYS */;
+INSERT INTO `quiz` VALUES (1,'PHP',10);
+/*!40000 ALTER TABLE `quiz` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz_taken`
+--
+
+DROP TABLE IF EXISTS `quiz_taken`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz_taken` (
+  `date_taken` date NOT NULL,
+  `points_accumulated` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `quiz_taken` int(11) NOT NULL,
+  KEY `quiz_taken_idx` (`quiz_taken`),
+  KEY `username_idx` (`username`),
+  CONSTRAINT `quiz_taken` FOREIGN KEY (`quiz_taken`) REFERENCES `quiz` (`quiz_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `accounts` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz_taken`
+--
+
+LOCK TABLES `quiz_taken` WRITE;
+/*!40000 ALTER TABLE `quiz_taken` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quiz_taken` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -56,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-24 20:16:41
+-- Dump completed on 2018-05-02 20:03:06
