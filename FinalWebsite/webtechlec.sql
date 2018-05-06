197651ab1e1d802d17749435dff0df947312fe12
erikatorio <2160120@slu.edu.ph>
2018-05-06 21:37:33 +0800

    changed choices table in the website database

--- FinalWebsite/webtechlec.sql
+++ FinalWebsite/webtechlec.sql
@@ -53,13 +53,15 @@ DROP TABLE IF EXISTS `choices`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `choices` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `choice` varchar(100) DEFAULT NULL,
-  `question_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `question_id_idx` (`question_id`),
-  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4;
+  `idchoices` int(11) NOT NULL AUTO_INCREMENT,
+  `a` varchar(45) NOT NULL,
+  `b` varchar(45) NOT NULL,
+  `c` varchar(45) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`idchoices`),
+  KEY `question_idx` (`question_id`),
+  CONSTRAINT `question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +70,7 @@ CREATE TABLE `choices` (
 
 LOCK TABLES `choices` WRITE;
 /*!40000 ALTER TABLE `choices` DISABLE KEYS */;
-INSERT INTO `choices` VALUES (1,'Personal Home Page',1),(2,'Hypertext Preprocessor',1),(3,'Process Home Preprocessor',1),(4,'1993',2),(5,'1994',2),(6,'1995',2),(7,'//',3),(8,'/',3),(9,'/*',3),(10,'Dollar sign symbol',4),(11,'Euro sign symbol',4),(12,'Peso sign symbol',4),(13,'Arithmetic',5),(14,'Logical',5),(15,'Comparison ',5),(16,'Assignment',5),(17,'All of the above',5),(18,'At the beginning',6),(19,'Inside the &ltHead&gt tag',6),(20,'At the bottom of the HTML ',6),(21,'Inside a &lt?php ?&gt notation',6),(22,'&ltphp&gt &lt/php&gt',7),(23,'&lt!php&gt &lt!php&gt',7),(24,'&lt?php ?&gt',7),(25,'&lt?php&gt',7),(26,'It is okay without a semi-colon',8),(27,'With semi colon',8),(28,'Both A and B',8),(29,'True',9),(30,'False',9),(31,'True',10),(32,'False',10),(33,'Java Servlet is a server side technology that runs on a web application server',11),(34,'Servlets are used to create dynamic web pages in a web application',11),(35,'Both of the above',11),(36,'request.getParameterValues()',12),(37,'request.getParameter()',12),(38,'request.getParameterNames()',12),(39,'start() method',13),(40,'init() method',13),(41,'startserv() method',13),(42,'request.getCookies()',14),(43,'cookies.getCookies()',14),(44,'get.requestCookies()',14),(45,'end() method',15),(46,'stop()method',15),(47,'destroy()method',15),(48,'Servlet Life Span',16),(49,'Servlet Life Cycle',16),(50,'Servlet Cycle',16),(51,'request.getMethod()',17),(52,'retrieve.getMethod()',17),(53,'method.getName()',17),(54,'Media Filters',18),(55,'Encryption Filters',18),(56,'Image Conversion Filters',18),(57,'error-handler',19),(58,'error-page',19),(59,'error-exception',19),(60,'service() method',20),(61,'perform() method',20),(62,'doTask() method',NULL);
+INSERT INTO `choices` VALUES (1,'Personal Hypertext Preprocessor','Personal Home Page','Hypertext Preprocessor',1),(2,'1993','1994','1995',2),(3,'//','<-- -->','/*',3),(4,'Dollar sign symbol','Euro sign symbol','Peso sign symbol',4),(5,'require() method','include() method','Both A and B',5),(6,'SuperGlobal','_SUPERGLOBAL','super_global',6),(7,'\"Hello\" . \"World\"','\"Hello\" + \"World\"','None of the above',7),(8,'Without a semicolon','With semicolon','Both A and B',8),(9,'nowdoc','heredoc','Both A and B',9),(10,'echo','print','cout',10),(11,'Java Servlet is a server side technology that','Servlets are used to create dynamic web pages','Both of the above',11),(12,'request.getParameterValues()','request.getParameter()','request.getParameterNames()',12),(13,'start() method','init() method','startserv() method',13),(14,'request.getCookies()','cookies.getCookies()','get.requestCookies()',14),(15,'end() method','stop()method','destroy()method',15),(16,'Servlet Life Span','Servlet Life Cycle','Servlet Cycle',16),(17,'request.getMethod()','retrieve.getMethod()','method.getName()',17),(18,'Media Filters','Encryption Filters','Image Conversion Filters',18),(19,'error-handler','error-page','error-exception',19),(20,'service() method','perform() method','doTask() method',20);
 /*!40000 ALTER TABLE `choices` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +98,7 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
-INSERT INTO `questions` VALUES (1,'What does PHP stand for?',1,'2'),(2,'When was PHP developed?',1,'2'),(3,'In PHP, how can you create a comment?',1,'1'),(4,'In creating a variable, the variable should start with ___ symbol',1,'1'),(5,'What are some kinds of operators in PHP that exists?',1,'5'),(6,'In creating a PHP inside of a HTML, where can you place the php script?',1,'4'),(7,'How do you start a php script or end it?',1,'3'),(8,'How can you end a php code in a line or a statement?',1,'2'),(9,'Is PHP an open source scripting language? ',1,'1'),(10,'It is allowed in php to contain a whitespace in the variable’s name',1,'2'),(11,'What are Servlets?',2,'3'),(12,'What is the method used to get the value of a form parameter?',2,'2'),(13,'This method is only called when the servlet is created',2,'2'),(14,'What is the method used to get cookies in servlet?',2,'1'),(15,'This method is only called when the life cycle of servlet ends',2,'3'),(16,'It is the entire process of servlet from its creation till the destruction',2,'2'),(17,'Which of the following method retrieves the name of HTTP method?',2,'3'),(18,'Which of the following is not a valid servlet filter?',2,'1'),(19,'It is an of element of web.xml that is used to specify the error handler in servlets',2,'2'),(20,'This method is called to perform the requested task',2,'1');
+INSERT INTO `questions` VALUES (1,'What does PHP stand for?',1,'c'),(2,'When was PHP developed?',1,'b'),(3,'In PHP, how can you create a comment?',1,'c'),(4,'In creating a variable, the variable should start with ___ symbol',1,'a'),(5,'Which method in PHP produces a fatal error when invoked',1,'a'),(6,'What is the format of PHP superglobals?',1,'b'),(7,'How do you concatenate strings in PHP',1,'a'),(8,'How can you end a php code in a line or a statement?',1,'b'),(9,'It is a syntax handler used for multi-line strings.',1,'b'),(10,'Which PHP command prints out a string with a return value?',1,'b'),(11,'What are Servlets?',2,'c'),(12,'What is the method used to get the value of a form parameter?',2,'b'),(13,'This method is only called when the servlet is created',2,'b'),(14,'What is the method used to get cookies in servlet?',2,'a'),(15,'This method is only called when the life cycle of servlet ends',2,'c'),(16,'It is the entire process of servlet from its creation till the destruction',2,'b'),(17,'Which of the following method retrieves the name of HTTP method?',2,'c'),(18,'Which of the following is not a valid servlet filter?',2,'a'),(19,'It is an of element of web.xml that is used to specify the error handler in servlets',2,'b'),(20,'This method is called to perform the requested task',2,'a');
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -166,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-06 11:55:57
+-- Dump completed on 2018-05-06 21:37:05
