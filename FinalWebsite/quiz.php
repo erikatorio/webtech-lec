5872154731e8bdb8def9fe121c0bb9b6cdb68a1e
erikatorio <2160120@slu.edu.ph>
2018-05-06 22:12:58 +0800

    added initial quiz.html file and quiz.php with javascript

--- FinalWebsite/quiz.php
+++ FinalWebsite/quiz.php
@@ -6,28 +6,6 @@ $query = "SELECT questions.question_id, question, questions.quiz_id, correctansw
 
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
-// $questionfield = array_fill_keys(array(question), null);
-
-// $choicekeys = array(a, b, c);
-// $choicefields = array_fill_keys($choice_keys, null);
-
-// $questions = array();
-// $choice = array();
-
-// while ($row = mysqli_fetch_assoc($result)) {
-//     $questionID = $row['question_id'];
-//     $choiceID = $row['id'];
-//     if(isset($questions[$questionID]['choices'])) {
-//         $questions[$questionID]['choices'][] = array_intersect_key($choiceID, $choicefields);
-//     } else {
-//         $questions[$questionID] = array_intersect_key($row, $questionfield);
-//         $questions[$questionID]['choices'] = array(array_intersect_key($, $choicefields));
-//     }
-// }
-
-// $questions = array_values($questions);
-// echo json_encode($questions);
-
 $questions = array();
 
 $questionID = 0;
@@ -42,11 +20,11 @@ while ($row = mysqli_fetch_assoc($result)) {
         $choiceIndex = -1;
         $questionID = $row['question_id'];
 
-        $questions[$questionIndex]['theQuestion'] = $row['question'];
+        $questions[$questionIndex]['question'] = $row['question'];
 
-        $questions[$questionIndex]['choices'] = array();
+        $questions[$questionIndex]['answers'] = array();
 
-        $questions[$questionIndex]['correct'] = $row['correctanswer'];
+        $questions[$questionIndex]['correct'] = $row['correctAnswer'];
     }
 
     if($choiceID != $row['idchoices']) {
@@ -54,7 +32,7 @@ while ($row = mysqli_fetch_assoc($result)) {
         $choiceID = $row['idchoices'];
         //$choicekeys = array(a, b, c);
 
-        $questions[$questionIndex]['choices'][$choiceIndex] = array(
+        $questions[$questionIndex]['answers'][$choiceIndex] = array(
             'a' => $row['a'],
             'b' => $row['b'],
             'c' => $row['c']
@@ -63,7 +41,116 @@ while ($row = mysqli_fetch_assoc($result)) {
 }
 $j = json_encode($questions);
 ?>
-<script type="text/javascript">
+<script>
 const q = <?php echo $j?>;
 console.log(q)
+(function() {
+    function buildQuiz() {
+      const output = [];
+  
+      q.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
+  
+        for (letter in currentQuestion.answers) {
+          answers.push(
+            `<label>
+               <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter}.
+                ${currentQuestion.answers[letter]}
+             </label>`
+          );
+        }
+  
+        output.push(
+          `<div class="slide">
+             <div class="question"> ${currentQuestion.question} </div>
+             <div class="answers"> ${answers.join("")} </div>
+           </div>`
+        );
+      })
+  
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults() {
+      const answerContainers = quizContainer.querySelectorAll(".answers");
+  
+      let correct = 0;
+  
+      q.forEach((currentQuestion, questionNumber) => { 
+        const answerContainer = answerContainers[questionNumber];
+        const selected = `input[name=question${questionNumber}]:checked`;
+        const userAns = (answerContainer.querySelector(selected) || {}).value;
+  
+        if (userAns === currentQuestion.correctAnswer) {
+          correct++;
+  
+          answerContainers[questionNumber].style.color = " #27ae60";
+        } else {
+          answerContainers[questionNumber].style.color = "red";
+        }
+      });
+      if (correct == 1) {
+        resultsContainer.innerHTML = `You correctly answered ${correct} question out of ${q.length} questions!`;
+      } else if (correct == 0) {
+        resultsContainer.innerHTML = `You have no correct answers!`;
+      } else {
+        resultsContainer.innerHTML = `You correctly answered ${correct} questions out of ${q.length} questions!`;
+      }
+      
+    }
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove("active-slide");
+      slides[n].classList.add("active-slide");
+      currentSlide = n;
+      
+      if (currentSlide === 0) {
+        previousButton.style.display = "none";
+      } else {
+        previousButton.style.display = "inline-block";
+      }
+      
+      if (currentSlide === slides.length - 1) {
+        nextButton.style.display = "none";
+        submitButton.style.display = "inline-block";
+        resetButton.style.display = "inline-block";
+      } else {
+        nextButton.style.display = "inline-block";
+        submitButton.style.display = "none";
+        resetButton.style.display = "none";
+      }
+    }
+  
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    function restartQuiz() {
+      location.reload();
+    }
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const resetButton = document.getElementById('reset');
+  
+    buildQuiz();
+  
+    const previousButton = document.getElementById('previous');
+    const nextButton = document.getElementById('next');
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+  
+    showSlide(0);
+  
+    resetButton.addEventListener("click", restartQuiz);
+    submitButton.addEventListener("click", showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+})();
 </script>
