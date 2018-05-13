b46d469cd6cf6f7b12a285f28ea5ffcc2af71c7f
erikatorio <2160120@slu.edu.ph>
2018-05-13 15:00:13 +0800

    fixed website file structure and integrated topic page

--- FinalWebsite/server/recordquiz.php
+++ FinalWebsite/server/recordquiz.php
@@ -41,7 +41,7 @@ if(isset($_GET['result']) and isset($_GET['id'])) {
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand ml-5" href="#">
+    <a class="navbar-brand ml-5" href="../pages/home.html">
       <img src="../image/Logo1x.png" alt="" id="logos">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -52,9 +52,6 @@ if(isset($_GET['result']) and isset($_GET['id'])) {
       <ul class="navbar-nav ml-auto mr-5">
         <li class="nav-item active">
           <a class="nav-link" href="../pages/home.html">Catalog<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Learn</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -68,14 +65,31 @@ if(isset($_GET['result']) and isset($_GET['id'])) {
       </ul>
     </div>
   </nav>
-  <div class="results mt-5">
-      <?php $result = $_GET['result'];
-        echo $result;
-      ?>
+  <div class="row mt-5">
+    <div class="col-sm-0 col-md-3"></div>
+    <div class="col-sm-0 col-md-6">
+      <div class="container track-card">
+        <div class="row">
+          <div class="col-4">
+            <div class="container status mt-3">
+                <h6 id="statusCode">Your <br><br> Score</h6>
+            </div>
+          </div>
+          <div class="col-8">
+            <h5 class="mt-3">You got:</h5>
+            <p class="text-justify" style="font-size: 50px;">
+            <?php $result = $_GET['result'];
+              echo $result;
+            ?>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-0 col-md-3"></div>
   </div>
-  <script src="../bootstrap/js/bootstrap.bundle.js"></script>
-  <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../bootstrap/js/bootstrap.js"></script>
-  <script src="../bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 </body>
 </html>
