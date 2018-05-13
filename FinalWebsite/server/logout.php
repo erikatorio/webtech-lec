96cd9e0bdc24b974b9533a8de5e5e8eaed794fcd
erikatorio <2160120@slu.edu.ph>
2018-05-13 14:13:10 +0800

    fixed website file strucuture

---
+++ FinalWebsite/server/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require('connect.php');
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+
+    $active = "UPDATE accounts SET status='0' WHERE username='$username'";
+    mysqli_query($link, $active);
+}
+
+unset($_SESSION['username']);
+header('Location: ../index.html');
